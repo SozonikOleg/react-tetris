62e6d8da357266b573179a00c21de2e243d55626
Aleh_Sazonik <aleh_sazonik@epam.com>
2020-12-22 19:30:08 +0300

    [part-2]: add tetrominos

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Cell from './Cell';
 
-const Stage = () => (
-    <div><Cell /></div>
+const Stage = ({stage}) => (
+    <div>
+        {stage.map( row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
+    </div>
 )
 
 export default Stage;
